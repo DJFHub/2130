fb81eccb93e767ebd769e097d04b1697b7dcb355
DJ <xiaolin619@gmail.com>
2018-04-09 01:05:10 -0500

    modified MyScrabble.c and client2still have bugs

--- MyScrabble.c
+++ MyScrabble.c
@@ -5,43 +5,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>//network system functions
-#include <netinet/in.h>// protocol & struct definition 
+#include <netinet/in.h>// protocol & struct definition
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define BUF_SIZE	1024
+#define BUF_SIZE	2
 
-void getNewBoard(); 
-void drawBoard(); 
-void makePlay(int x, int y, char* c); 
+void getNewBoard();
+void drawBoard();
+void makePlay(int x, int y, char* c);
 int SCRABBLE_LETTER_VALUES(char letterValue);
-void tcpServer(); 
-int createTCPServerSocket(unsigned short port); 
+void tcpServer();
+int createTCPServerSocket(unsigned short port);
 void  handleTCPClient(int client_socket);
-int acceptTCPConnection(int server_socket); 
+int acceptTCPConnection(int server_socket);
 
 /*
-* Services the server should provide: 
-*   Draw the board which would include placing new moves  
-*   on the board 
-*   manage the playing, giving each client a turn 
-*   or allowing them to quit 
+* Services the server should provide:
+*   Draw the board which would include placing new moves
+*   on the board
+*   manage the playing, giving each client a turn
+*   or allowing them to quit
 *
 */
 int k,j;
-int const NUM_RANGE=8;
+#define NUM_RANGE 8
 //global declaration structure grid
 char * grid[NUM_RANGE][NUM_RANGE];
-int running = 1 ; 
+int running = 1 ;
 
 int main()
 {
 
-//calling the functions 
-    tcpServer(); 
-    
+//calling the functions
+    tcpServer();
+
     //making two plays
-    //makePlay(3,7,"a");    
-    //makePlay(4,5,"b");    
-    
+    //makePlay(3,7,"a");
+    //makePlay(4,5,"b");
+
     //redrawing the board with plays shown
     //drawBoard();
 
@@ -49,7 +52,7 @@ int main()
 }
 
 void getNewBoard(){
-        //creates a brand new blank board. Returns a pointer to the array 
+        //creates a brand new blank board. Returns a pointer to the array
         for (j = 0; j < NUM_RANGE; j++){
             for (k = 0; k < NUM_RANGE; k++)
             {
@@ -68,7 +71,7 @@ void drawBoard(){
     printf("%s\n",NLINE);
     printf("%s\n",HLINE);
     for (j = 0; j < NUM_RANGE; j++)
-    {  
+    {
         printf("%s\n",VLINE);
         printf("%d ",j+1);
         for (k = 0; k < NUM_RANGE; k++)
@@ -86,7 +89,7 @@ void drawBoard(){
     return;
 }//EndFunction drawBoard
 
-   
+
 void makePlay(int x, int y, char* c){
    //Arrays are zero indexed but our grid starts at index 1
    grid[x-1][y-1]=c;
@@ -95,135 +98,135 @@ void makePlay(int x, int y, char* c){
 
 int SCRABBLE_LETTER_VALUES(char letterValue)
 {
-    int value; 
+    int value;
     switch(letterValue)
     {
-        case 'A': 
-        value = 1; 
-        break; 
-        case 'B': 
-        value = 3; 
-        break; 
-        case 'C': 
-        value = 3; 
-        break; 
-        case 'D': 
-        value = 2; 
-        break; 
-        case 'E': 
-        value = 1; 
-        break; 
-        case 'F': 
-        value = 4; 
-        break; 
-        case 'G': 
-        value = 2; 
-        break; 
-        case 'H': 
-        value = 4; 
-        break; 
-        case 'I': 
-        value = 1; 
-        break; 
-        case 'J': 
-        value = 8; 
-        break; 
-        case 'K': 
-        value = 5; 
-        break; 
-        case 'L':  
-        value = 1; 
-        break; 
-        case 'M': 
-        value = 3; 
-        break; 
-        case 'N': 
-        value = 1; 
-        break; 
+        case 'A':
+        value = 1;
+        break;
+        case 'B':
+        value = 3;
+        break;
+        case 'C':
+        value = 3;
+        break;
+        case 'D':
+        value = 2;
+        break;
+        case 'E':
+        value = 1;
+        break;
+        case 'F':
+        value = 4;
+        break;
+        case 'G':
+        value = 2;
+        break;
+        case 'H':
+        value = 4;
+        break;
+        case 'I':
+        value = 1;
+        break;
+        case 'J':
+        value = 8;
+        break;
+        case 'K':
+        value = 5;
+        break;
+        case 'L':
+        value = 1;
+        break;
+        case 'M':
+        value = 3;
+        break;
+        case 'N':
+        value = 1;
+        break;
         case 'O':
-        value = 1; 
-        break; 
+        value = 1;
+        break;
         case 'P':
-        value = 3; 
-        break; 
+        value = 3;
+        break;
         case 'Q':
-        value = 10; 
-        break; 
-        case 'R': 
-        value = 1; 
-        break; 
-        case 'S': 
-        value = 1; 
-        break; 
-        case 'T': 
-        value = 1; 
-        break; 
-        case 'U': 
-        value = 1; 
-        break; 
-        case 'V': 
-        value = 4; 
-        break; 
-        case 'W': 
-        value = 4; 
-        break; 
-        case 'X': 
-        value = 8; 
-        break; 
-        case 'Y': 
-        value = 4; 
-        break; 
-        case 'Z': 
-        value = 10; 
-        break; 
+        value = 10;
+        break;
+        case 'R':
+        value = 1;
+        break;
+        case 'S':
+        value = 1;
+        break;
+        case 'T':
+        value = 1;
+        break;
+        case 'U':
+        value = 1;
+        break;
+        case 'V':
+        value = 4;
+        break;
+        case 'W':
+        value = 4;
+        break;
+        case 'X':
+        value = 8;
+        break;
+        case 'Y':
+        value = 4;
+        break;
+        case 'Z':
+        value = 10;
+        break;
     }
-    return value; 
+    return value;
 }
-// TCP Server Functions 
+// TCP Server Functions
 void tcpServer()
 {
     int *serverSock; //aka the descriptor for the sock
-    int maxDesc;  //max socket descriptor value 
+    int maxDesc;  //max socket descriptor value
     fd_set sockSet; // add a descriptor to the vector table
-    int const noPort = 2; 
-    int c1_port = 60000, c2_port = 60001; 
-   
+    int const noPort = 2;
+    int c1_port = 60000, c2_port = 60001;
+
     getNewBoard();
     //Displaying empty board
     printf("\n%s\n\n","Printing an empty board....");
     drawBoard();
 
     //allocate memory for the number sockets for the incoming connections
-    serverSock = (int *) malloc( noPort * sizeof(int)); 
-    
-    // creating two server sockets so that the clients can connect 
-    // to one each 
+    serverSock = (int *) malloc( noPort * sizeof(int));
+
+    // creating two server sockets so that the clients can connect
+    // to one each
     serverSock[0] = createTCPServerSocket(c1_port);
     maxDesc = serverSock[0];
     serverSock[1] = createTCPServerSocket(c2_port);
     if (maxDesc < serverSock[1])
         maxDesc = serverSock[1];
-    
+
     while(running)
     {
-        FD_ZERO(&sockSet); 
-        FD_SET(&serverSock[0], &sockSet);//descriptor to the vector 
-        FD_SET(&serverSock[1], &sockSet);
+        FD_ZERO(&sockSet);
+        FD_SET(serverSock[0], &sockSet);//descriptor to the vector
+        FD_SET(serverSock[1], &sockSet);
 
         if (select(maxDesc+1,&sockSet,NULL,NULL,NULL) < 0)
-            printf("\nselect() failed\n");
+            printf("\nselect() in tcpServer failed\n");
         else
         {
-            //FD_ISSET test whether the fd is apart of the set 
+            //FD_ISSET test whether the fd is apart of the set
             for (int i ;  i < 2 ; i++)
             {
                 if (FD_ISSET(serverSock[i], &sockSet))
                 {
-                    handleTCPClient(acceptTCPConnection(serverSock[i])); 
+                    handleTCPClient(acceptTCPConnection(serverSock[i]));
                     if (running)
                     {
                         printf("\n%s\n\n","RePrinting board after plays....");
-                        drawBoard(); 
+                        drawBoard();
                     }
                 }
             }
@@ -231,82 +234,85 @@ void tcpServer()
         }
 
     }
-    close(serverSock[0]); 
-    close(serverSock[1]); 
+    close(serverSock[0]);
+    close(serverSock[1]);
 
-    free(serverSock); 
+    free(serverSock);
 
-    exit(0); 
+    exit(0);
 
 }
 int createTCPServerSocket(unsigned short port)
 {
-    int sock; 
+    int sock;
     struct sockaddr_in serv_addr; //hold the local addresss
 
     //create the socket for the incoming connections
     if ( (sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 )
-        printf("\nsocket() failed\n"); 
-    
-    memset(&serv_addr, 0 , sizeof(serv_addr)); 
-    serv_addr.sin_family = AF_INET; 
+        printf("\nsocket() failed\n");
+
+    memset(&serv_addr, 0 , sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); //*
-    serv_addr.sin_port = htons(port); 
+    serv_addr.sin_port = htons(port);
 
     //bind the local addresss
     if (bind(sock, (struct sockaddr *)&serv_addr,sizeof(serv_addr)))
-        printf("\nbind() failed\n");
-    
+        printf("\nbind() in createTCPServerSocket failed\n");
+
     if ( listen(sock, 5) < 0)
-        printf("\nlisten() failed\n"); 
-    
-    return sock; 
+        printf("\nlisten() in createTCPServerSocket failed\n");
+
+    return sock;
 }
 
 void  handleTCPClient(int client_socket)
 {
-    char  x_cordinate[BUF_SIZE], y_cordinate[BUF_SIZE] 
-    , letter[BUF_SIZE], playing[BUF_SIZE];
-    int  recvMsgSize; 
-    
-    //recieve message from client 
-     if (recvMsgSize = recv(client_socket, playing, BUF_SIZE, 0)!= recvMsgSize)
-        printf("\nrecv() failed\n");
-    playing[BUF_SIZE] = '\0'; 
-    if (recvMsgSize = recv(client_socket, x_cordinate, BUF_SIZE, 0)!= recvMsgSize)
-        printf("\nrecv() failed\n");
-    x_cordinate[BUF_SIZE] = '\0';
-    if (recvMsgSize = recv(client_socket, y_cordinate, BUF_SIZE, 0)!= recvMsgSize)
-        printf("\nrecv() failed\n");
-    y_cordinate[BUF_SIZE] = '\0';
-    if (recvMsgSize = recv(client_socket,letter, BUF_SIZE, 0)!= recvMsgSize)
-        printf("\nrecv() failed\n");
-    letter[BUF_SIZE] = '\0';
+    char  x_cordinate, y_cordinate
+    , letter, playing;
+    int  recvMsgSize;
+    struct sockaddr_in	remote_addr;
+    int len  = sizeof(remote_addr);
+    //recieve message from client
+     recvMsgSize = recvfrom(client_socket,x_cordinate,sizeof(char ),MSG_WAITALL,(struct sockaddr *)&remote_addr,&len);
+     if (recvMsgSize > 0)
+        //x_cordinate[recvMsgSize] = '\0';
 
-        
-    //we need to make the play 
-    if (strcmp("n",playing)!=0)
-     makePlay(atoi(x_cordinate),atoi(y_cordinate), &letter);
+     recvMsgSize=recvfrom(client_socket,y_cordinate,sizeof(char ),MSG_WAITALL,(struct sockaddr *)&remote_addr,&len);
+     if (recvMsgSize > 0)
+        //y_cordinate[recvMsgSize] = '\0';
+     recvMsgSize=recvfrom(client_socket,letter,sizeof(char ),MSG_WAITALL,(struct sockaddr *)&remote_addr,&len);
+     if (recvMsgSize > 0)
+        //letter[recvMsgSize] = '\0';
+     recvMsgSize=recvfrom(client_socket,playing,sizeof(char ),MSG_WAITALL,(struct sockaddr *)&remote_addr,&len);
+     if (recvMsgSize > 0)
+      //playing[recvMsgSize] = '\0';
+
+      printf("%c %c %c %c",x_cordinate,y_cordinate,letter,playing);
+    //we need to make the play
+    if (playing !='n')
+     makePlay( (x_cordinate - '0'), (y_cordinate - '0'),letter);
     else
-        running = 0 ; 
+        running = 0 ;
 
-     close(client_socket); 
+     close(client_socket);
+
 }
 
 int acceptTCPConnection(int server_socket)
 {
 
-    int client_socket; 
-    struct sockaddr_in clnt_addr ; 
-    unsigned int len; 
+    int client_socket;
+    struct sockaddr_in clnt_addr ;
+    unsigned int len;
 
-    //size of I/O parameter 
+    //size of I/O parameter
     len = sizeof(clnt_addr);
- 
+
     if ((client_socket = accept(server_socket,(struct sockaddr *)&clnt_addr,&len))< 0)
-        printf("\naccept() failed\n");
-    
-    //now the client socket is connected 
+        printf("\naccept() in acceptTCPConnection failed\n");
+
+    //now the client socket is connected
     //printf("Handling client %s\n", inet_ntoa(clnt_addr.sin_addr)) ;
     return client_socket;
 }
--- client1.c
+++ client1.c
@@ -1,55 +1,58 @@
 #include <stdio.h>
 #include <sys/types.h>//system type definitions
 #include <sys/socket.h>//network system functions
-#include <netinet/in.h>// protocol & struct definition 
+#include <netinet/in.h>// protocol & struct definition
 #include <string.h>
 #include <unistd.h>
 
 int main()
 {
-    int i, sock; 
-    char x_cordinate[80], y_cordinate[80]; 
-    char still_playing[80]; 
-    struct sockaddr_in  addr_send; 
-    unsigned short  server_port = 60000;  
-    
+    int i, sock;
+    char x_cordinate[80], y_cordinate[80], letter[80];
+    char still_playing[80];
+    struct sockaddr_in  addr_send;
+    unsigned short  server_port = 60000;
 
-    //creating a socket 
+
+    //creating a socket
     sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sock < 0)
     {
-        printf("Socket() failed\n"); 
+        printf("Socket() failed\n");
     }
 
-   
 
-    //make local address structure 
-    memset(&addr_send, 0, sizeof(addr_send));  //clear structure 
+
+    //make local address structure
+    memset(&addr_send, 0, sizeof(addr_send));  //clear structure
     addr_send.sin_family = PF_INET; //family address
     addr_send.sin_port = htons(server_port);
- 
-    //connect to server 
-    i = connect(sock, &addr_send, sizeof(addr_send)); 
+
+    //connect to server
+    i = connect(sock, (struct sockaddr *)&addr_send, sizeof(addr_send));
     if (i < 0)
-        printf("connect() failed\n"); 
-    do 
+        printf("connect() failed\n");
+    do
     {
         printf("\nEnter x-position: ");
-        scanf("%s",x_cordinate); 
+        scanf("%s",x_cordinate);
         printf("\nEnter y-position: ");
-        scanf("%s",y_cordinate); 
-        printf("\nContinue Playing (y/n) ?  "); 
-        scanf("%s",still_playing); 
+        scanf("%s",y_cordinate);
+        printf("\nEnter letter: ");
+        scanf("%s",letter);
+        printf("\nContinue Playing (y/n) ?  ");
+        scanf("%s",still_playing);
 
         send(sock,still_playing,strlen(still_playing), 0);
         send(sock,x_cordinate,strlen(x_cordinate), 0);
         send(sock,y_cordinate,strlen(y_cordinate), 0);
+        send(sock,letter,strlen(letter), 0);
 
-    }while(strcmp("n",still_playing)!= 0); 
+    }while(strcmp("n",still_playing)!= 0);
 
-   //close commmunication 
-    i = close (sock); 
+   //close commmunication
+    i = close (sock);
     if (i < 0 )
-        printf("close() failed\n"); 
-    return 0; 
+        printf("close() failed\n");
+    return 0;
 }
